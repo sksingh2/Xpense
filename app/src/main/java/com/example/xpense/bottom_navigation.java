107d70ca02aa82fff65d2bd86e89b28c7428fabe
shiva <shivamtza1y@gmail.com>
2022-04-24 21:30:15 +0530

    Merge remote-tracking branch 'origin/master'
    BOTTOM NAVIGATION

--- app/src/main/java/com/example/xpense/bottom_navigation.java
+++ app/src/main/java/com/example/xpense/bottom_navigation.java
@@ -1,14 +1,60 @@
 package com.example.xpense;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class bottom_navigation extends AppCompatActivity {
+
+
+    BottomNavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+        //THIS LINE HIDE STATUSBAR
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.botom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container,new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_friends:
+                        fragment = new FriendFragment();
+                        break;
+
+                    case R.id.nav_groups:
+                        fragment = new GroupsFragment();
+                        break;
+
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container,fragment).commit();
+
+                return true;
+            }
+        });
+
     }
 }
