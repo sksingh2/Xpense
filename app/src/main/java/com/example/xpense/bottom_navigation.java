b5d63ee9b7625517bf7412e814a578800207bc42
shiva <shivamtza1y@gmail.com>
2022-04-24 11:55:31 +0530

    xpense app

---
+++ app/src/main/java/com/example/xpense/bottom_navigation.java
@@ -0,0 +1,14 @@
+package com.example.xpense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bottom_navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
