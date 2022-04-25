40ea1afe785343f0476758adc4c9659bd01930e3
owais <owais.khanabc12@gmail.com>
2022-04-25 12:07:14 +0530

    new signup

--- app/src/main/java/com/example/xpense/Sign_Up.java
+++ app/src/main/java/com/example/xpense/Sign_Up.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LOGIN_PAGE extends AppCompatActivity {
+public class Sign_Up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_sign_up);
     }
 }
