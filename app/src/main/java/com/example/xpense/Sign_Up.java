57350adc4b80ea4e2d1ede4b7dc6ab6f5606ddc4
owais <owais.khanabc12@gmail.com>
2022-04-26 14:28:43 +0530

    small commit in sign up

--- app/src/main/java/com/example/xpense/Sign_Up.java
+++ app/src/main/java/com/example/xpense/Sign_Up.java
@@ -2,7 +2,6 @@ package com.example.xpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +13,9 @@ public class Sign_Up extends AppCompatActivity {
 
     Button get_otp, new_user, sign_up;
     EditText phone_no, in_otp;
-    String phone;
+    String temp;
     int temp_otp = 1234, otp;
+    long phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,53 +25,53 @@ public class Sign_Up extends AppCompatActivity {
         sign_up = findViewById(R.id.sign_up_log);
         phone_no = findViewById(R.id.phone_no);
         in_otp = findViewById(R.id.otp);
-        get_otp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                phone = phone_no.getText().toString();
-                if (phone.length() != 10)
-                {
-                   Toast invalid_no = Toast.makeText(getApplicationContext(), "Invalid Number",Toast.LENGTH_SHORT);
-                         invalid_no.show();
-                   /* LONG SYNTAX FOR TOAST
-                    Context context = getApplicationContext();
-                    CharSequence text = "Invalid Number";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast invalid_number = Toast.makeText(context, text, duration);
-                    invalid_number.show();**/
-                }
-                else
-                {
-                    Toast otp_send = Toast.makeText(getApplicationContext(),"Otp send",Toast.LENGTH_SHORT);
-                    otp_send.show();
-                }
-
-            } });
-
-        sign_up.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //temp_otp="1234";
-                otp = Integer.parseInt(in_otp.getText().toString());
-                if (otp == temp_otp)
-                {
-                    Intent verified = new Intent(Sign_Up.this,HomeFragment.class );
-                    startActivity(verified);
-                }
-                else
-                {
-                    Toast invalid_otp = Toast.makeText(getApplication(), "Invalid Otp", Toast.LENGTH_SHORT);
-                    invalid_otp.show();
-                }
-
-
+        get_otp.setOnClickListener(v -> {
+            temp=phone_no.getText().toString();
+            if (temp.length() != 10 || temp.isEmpty()) {
+                Toast invalid_no = Toast.makeText(getApplicationContext(), "Enter a valid Number", Toast.LENGTH_SHORT);
+                invalid_no.show();
+               /* LONG SYNTAX FOR TOAST
+                Context context = getApplicationContext();
+                CharSequence text = "Invalid Number";
+                int duration = Toast.LENGTH_SHORT;
+                Toast invalid_number = Toast.makeText(context, text, duration);
+                invalid_number.show();**/
+            }
+            else
+            {
+                phone =Long.parseLong(temp);
+                Toast otp_send = Toast.makeText(getApplicationContext(),"Otp send",Toast.LENGTH_SHORT);
+                otp_send.show();
             }
         });
-        new_user.setOnClickListener(new View.OnClickListener()
+
+        sign_up.setOnClickListener(v -> {
+            //temp_otp="1234";
+            otp = Integer.parseInt(in_otp.getText().toString());
+            if (otp == temp_otp)
+            {
+                Intent verified = new Intent(Sign_Up.this,HomeFragment.class );
+                startActivity(verified);
+            }
+            else
+            {
+                Toast invalid_otp = Toast.makeText(getApplication(), "Invalid Otp", Toast.LENGTH_SHORT);
+                invalid_otp.show();
+            }
+
+
+        });
+        new_user.setOnClickListener(v -> {
+            Intent redirect_home = new Intent(Sign_Up.this,SigninPage.class );
+            startActivity(redirect_home);
+        });
+        /*new_user.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v){
                 Intent redirect_home = new Intent(Sign_Up.this,SigninPage.class );
                 startActivity(redirect_home);
             }
-        });
+        });**/
         }
     }
 
